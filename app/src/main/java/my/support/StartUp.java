ec6bfffe2384bf0c1893b05368b83197cd2961af
pankajnimgade <pankaj.nimgade@gmail.com>
2016-03-04 10:22:40 +0530

    initial commit

---
+++ app/src/main/java/my/support/StartUp.java
@@ -0,0 +1,14 @@
+package my.support;
+
+import android.app.Application;
+
+/**
+ * Created by Pankaj Nimgade on 27-02-2016.
+ */
+public class StartUp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
